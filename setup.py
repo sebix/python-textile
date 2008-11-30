5925226bbd1db9acce51ba36a537163c316e9514
Jason Samsa <jsamsa@gmail.com>
2008-11-29 18:04:02 -0600

    distutils configuration for pushing to pypi

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+
+setup(name='textile',
+      version='2.1.0',
+      description='This is Textile. A Humane Web Text Generator.',
+      author='Jason Samsa',
+      author_email='jsamsa@gmail.com',
+      url='http://loopcore.com/python-textile/',
+      py_modules=['textile'],
+      platforms = ['any'],
+      license = ['BSD'],
+      long_description = """Textile is a XHTML generator using a simple markup developed by Dean Allen."""
+      )
