8c0e9b85ec008378ede77248c1e5c84626afacef
Kurt Raschke <kurt@kurtraschke.com>
2009-08-23 14:21:35 -0400

    Refactor to use setuptools.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+import ez_setup
+ez_setup.use_setuptools()
 
-from distutils.core import setup
-
+from setuptools import setup
 
 setup(name='textile',
       version='2.1.3',
@@ -16,5 +17,9 @@ setup(name='textile',
       long_description = """
         Textile is a XHTML generator using a simple markup developed by Dean Allen.
         Python 2.4 users will need to install the uuid module
-        """
+        """,
+
+      setup_requires=['nose>=0.11', 'coverage>=3.0.1'],
+      test_suite = 'nose.collector',
+      tests_require = ['nose']
       )
