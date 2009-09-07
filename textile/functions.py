8e6f2f7d8f579c179156a093b5f611df9ef59a04
Kurt Raschke <kurt@kurtraschke.com>
2009-09-07 12:40:55 -0400

    Remove support for sanitization due to removal of sgmllib in Python 3.0.

--- textile/functions.py
+++ textile/functions.py
@@ -59,7 +59,6 @@ POSSIBILITY OF SUCH DAMAGE.
 import re
 import uuid
 from urlparse import urlparse
-import sgmllib
 
 def _normalize_newlines(string):
     import re
@@ -99,141 +98,6 @@ def getimagesize(url):
                 return 'width="%i" height="%i"' % p.image.size
     except (IOError, ValueError):
         return None
-
-# PyTextile can optionally sanitize the generated XHTML,
-# which is good for weblog comments. This code is from
-# Mark Pilgrim's feedparser.
-class _BaseHTMLProcessor(sgmllib.SGMLParser):
-    elements_no_end_tag = ['area', 'base', 'basefont', 'br', 'col', 'frame', 'hr',
-      'img', 'input', 'isindex', 'link', 'meta', 'param']
-
-    def __init__(self):
-        sgmllib.SGMLParser.__init__(self)
-
-    def reset(self):
-        self.pieces = []
-        sgmllib.SGMLParser.reset(self)
-
-    def normalize_attrs(self, attrs):
-        # utility method to be called by descendants
-        attrs = [(k.lower(), sgmllib.charref.sub(lambda m: unichr(int(m.groups()[0])), v).strip()) for k, v in attrs]
-        attrs = [(k, k in ('rel', 'type') and v.lower() or v) for k, v in attrs]
-        return attrs
-
-    def unknown_starttag(self, tag, attrs):
-        # called for each start tag
-        # attrs is a list of (attr, value) tuples
-        # e.g. for <pre class="screen">, tag="pre", attrs=[("class", "screen")]
-        strattrs = "".join([' %s="%s"' % (key, value) for key, value in attrs])
-        if tag in self.elements_no_end_tag and self.html_type == 'xhtml':
-            self.pieces.append("<%(tag)s%(strattrs)s />" % locals())
-        else:
-            self.pieces.append("<%(tag)s%(strattrs)s>" % locals())
-
-    def unknown_endtag(self, tag):
-        # called for each end tag, e.g. for </pre>, tag will be "pre"
-        # Reconstruct the original end tag.
-        if tag not in self.elements_no_end_tag or self.html_type == 'html':
-            self.pieces.append("</%(tag)s>" % locals())
-
-    def handle_charref(self, ref):
-        # called for each character reference, e.g. for "&#160;", ref will be "160"
-        # Reconstruct the original character reference.
-        self.pieces.append("&#%(ref)s;" % locals())
-
-    def handle_entityref(self, ref):
-        # called for each entity reference, e.g. for "&copy;", ref will be "copy"
-        # Reconstruct the original entity reference.
-        self.pieces.append("&%(ref)s;" % locals())
-
-    def handle_data(self, text):
-        # called for each block of plain text, i.e. outside of any tag and
-        # not containing any character or entity references
-        # Store the original text verbatim.
-        self.pieces.append(text)
-
-    def handle_comment(self, text):
-        # called for each HTML comment, e.g. <!-- insert Javascript code here -->
-        # Reconstruct the original comment.
-        self.pieces.append("<!--%(text)s-->" % locals())
-
-    def handle_pi(self, text):
-        # called for each processing instruction, e.g. <?instruction>
-        # Reconstruct original processing instruction.
-        self.pieces.append("<?%(text)s>" % locals())
-
-    def handle_decl(self, text):
-        # called for the DOCTYPE, if present, e.g.
-        # <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        #     "http://www.w3.org/TR/html4/loose.dtd">
-        # Reconstruct original DOCTYPE
-        self.pieces.append("<!%(text)s>" % locals())
-
-    def output(self):
-        """Return processed HTML as a single string"""
-        return "".join(self.pieces)
-
-
-class _HTMLSanitizer(_BaseHTMLProcessor):
-    acceptable_elements = ['a', 'abbr', 'acronym', 'address', 'area', 'b', 'big',
-      'blockquote', 'br', 'button', 'caption', 'center', 'cite', 'code', 'col',
-      'colgroup', 'dd', 'del', 'dfn', 'dir', 'div', 'dl', 'dt', 'em', 'fieldset',
-      'font', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'input',
-      'ins', 'kbd', 'label', 'legend', 'li', 'map', 'menu', 'ol', 'optgroup',
-      'option', 'p', 'pre', 'q', 's', 'samp', 'select', 'small', 'span', 'strike',
-      'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th',
-      'thead', 'tr', 'tt', 'u', 'ul', 'var']
-
-    acceptable_attributes = ['abbr', 'accept', 'accept-charset', 'accesskey',
-      'action', 'align', 'alt', 'axis', 'border', 'cellpadding', 'cellspacing',
-      'char', 'charoff', 'charset', 'checked', 'cite', 'class', 'clear', 'cols',
-      'colspan', 'color', 'compact', 'coords', 'datetime', 'dir', 'disabled',
-      'enctype', 'for', 'frame', 'headers', 'height', 'href', 'hreflang', 'hspace',
-      'id', 'ismap', 'label', 'lang', 'longdesc', 'maxlength', 'media', 'method',
-      'multiple', 'name', 'nohref', 'noshade', 'nowrap', 'prompt', 'readonly',
-      'rel', 'rev', 'rows', 'rowspan', 'rules', 'scope', 'selected', 'shape', 'size',
-      'span', 'src', 'start', 'summary', 'tabindex', 'target', 'title', 'type',
-      'usemap', 'valign', 'value', 'vspace', 'width']
-
-    unacceptable_elements_with_end_tag = ['script', 'applet']
-
-    # This if for MathML.
-    mathml_elements = ['math', 'mi', 'mn', 'mo', 'mrow', 'msup']
-    mathml_attributes = ['mode', 'xmlns']
-
-    acceptable_elements = acceptable_elements + mathml_elements
-    acceptable_attributes = acceptable_attributes + mathml_attributes
-
-    def reset(self):
-        _BaseHTMLProcessor.reset(self)
-        self.unacceptablestack = 0
-
-    def unknown_starttag(self, tag, attrs):
-        if not tag in self.acceptable_elements:
-            if tag in self.unacceptable_elements_with_end_tag:
-                self.unacceptablestack += 1
-            return
-        attrs = self.normalize_attrs(attrs)
-        attrs = [(key, value) for key, value in attrs if key in self.acceptable_attributes]
-        _BaseHTMLProcessor.unknown_starttag(self, tag, attrs)
-
-    def unknown_endtag(self, tag):
-        if not tag in self.acceptable_elements:
-            if tag in self.unacceptable_elements_with_end_tag:
-                self.unacceptablestack -= 1
-            return
-        _BaseHTMLProcessor.unknown_endtag(self, tag)
-
-    def handle_pi(self, text):
-        pass
-
-    def handle_decl(self, text):
-        pass
-
-    def handle_data(self, text):
-        if not self.unacceptablestack:
-            _BaseHTMLProcessor.handle_data(self, text)
-
 
 # PyTextile can optionally validate the generated
 # XHTML code using either mxTidy or uTidyLib.
@@ -320,7 +184,7 @@ class Textile(object):
         self.rel = ''
         self.html_type='xhtml'
 
-    def textile(self, text, rel=None, validate=False, sanitize=False, head_offset=0, html_type='xhtml'):
+    def textile(self, text, rel=None, validate=False, head_offset=0, html_type='xhtml'):
         """
         >>> import textile
         >>> textile.textile('some textile')
@@ -343,11 +207,6 @@ class Textile(object):
 
         text = self.retrieve(text)
 
-        # Sanitize?
-        if sanitize:
-            p = _HTMLSanitizer()
-            p.feed(text)
-            text = p.output()
 
         # Validate output.
         if _tidy and validate:
@@ -1122,7 +981,6 @@ def textile(text, **args):
     """
     this function takes additional parameters:
     validate - perform mxTidy or uTidyLib validation (default: False)
-    sanitize - sanitize output good for weblog comments (default: False)
     head_offset - offset to apply to heading levels (default: 0)
     html_type - 'xhtml' or 'html' style tags (default: 'xhtml')
     """
