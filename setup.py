719a2dde47ade7b2577cff5edb9d87e5b81912bd
Kurt Raschke <kurt@kurtraschke.com>
2009-12-15 15:21:32 -0500

    Getting back to setuptools; restoring setup.cfg for nose.

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
+import ez_setup
+ez_setup.use_setuptools()
 
-from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 setup(name='textile',
-      version='2.1.4',
+      version='2.1.3',
       description='This is Textile. A Humane Web Text Generator.',
       author='Jason Samsa',
       author_email='jsamsa@gmail.com',
       url='http://loopcore.com/python-textile/',
-      py_modules=['textile.functions'],
+      packages = find_packages(),
       platforms = ['any'],
       license = ['BSD'],
       long_description = """
         Textile is a XHTML generator using a simple markup developed by Dean Allen.
         Python 2.4 users will need to install the uuid module
-        """
+        """,
+
+      setup_requires=['nose>=0.11', 'coverage>=3.0.1'],
+      test_suite = 'nose.collector',
+      tests_require = ['nose']
       )
