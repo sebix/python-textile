c01b14b62bec83d7f4ab042ea90f78bbac28992f
Chris Drackett <chris@chrisdrackett.com>
2010-06-29 14:58:41 -0500

    reorg and making installable

--- functions.py
+++ functions.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
-"""
-PyTextile
-
-A Humane Web Text Generator
-"""
-
-__version__ = '2.1.3'
-
-__date__ = '2009/02/07'
 
 __copyright__ = """
 Copyright (c) 2009, Jason Samsa, http://jsamsa.com/
@@ -22,37 +13,6 @@ Thanks to Carlo Zottmann <carlo@g-blog.net> for refactoring
 Textile's procedural code into a class framework
 
 Additions and fixes Copyright (c) 2006 Alex Shiels http://thresholdstate.com/
-
-"""
-
-__license__ = """
-L I C E N S E
-=============
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice,
-  this list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* Neither the name Textile nor the names of its contributors may be used to
-  endorse or promote products derived from this software without specific
-  prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
 
 """
 
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-textile',
+    version='2.1.3',
+    description='Make type not look like crap (for django apps)',
+    author='Mark Pilgrim, Roberto A. F. De Almeida, Alex Shiels, Jason Samsa, Chris Drackett',
+    author_email='chris@shelfworthy.com',
+    url='http://github.com/chrisdrackett/python-textile',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers :: Designers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
