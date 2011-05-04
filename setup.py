2153a99e6b46c009e18c267f5e43879f8b2affc7
Chris Drackett <chris@chrisdrackett.com>
2011-05-04 11:25:04 +0900

    updating package info.

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+version = '2.1.5'
 
 setup(
     name='python-textile',
-    version='2.1.5',
-    description='Make type not look like crap (for django apps)',
-    author='Mark Pilgrim, Roberto A. F. De Almeida, Alex Shiels, Jason Samsa, Chris Drackett, Kurt Raschke',
-    author_email='kurt@kurtraschke.com',
-    url='http://github.com/kurtraschke/python-textile',
-    packages = [
-        "textile",
-        "textile.tools",
-        "textile.tests",
-    ],
+    version=version,
+    description='Textile processing for python.',
+    author='Chris Drackett',
+    author_email='chris@chrisdrackett.com',
+    url='http://github.com/chrisdrackett/python-textile',
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 1',
         'Environment :: Web Environment',
@@ -19,8 +17,11 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Utilities'
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    keywords='textile,text',
     test_suite = 'nose.collector',
-    tests_require = ['nose']
+    tests_require = ['nose'],
+    include_package_data=True,
+    zip_safe=False,
 )
