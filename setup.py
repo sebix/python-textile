eb83137395456bf6c34739a1d384b039d911f12b
ArmstrongJ <approximatrix@gmail.com>
2011-12-12 09:22:19 -0500

    Added 2to3 support to setup for compatability with Python 3.x

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
+import sys
 
 version = '2.1.5'
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
 
 setup(
     name='textile',
@@ -24,4 +29,5 @@ setup(
     tests_require = ['nose'],
     include_package_data=True,
     zip_safe=False,
+    **extra
 )
