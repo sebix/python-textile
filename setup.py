44c52544c7bbe3657f48cf41ff4cf87981eaa1dd
Dennis Burke <dennisburke@prodigy.net>
2014-10-14 09:27:52 -0400

    more testing against travis.

--- setup.py
+++ setup.py
@@ -2,46 +2,47 @@ from setuptools import setup, find_packages
 import os
 import sys
 
-install_requires = ['regex']
 
-try:
-    from collections import OrderedDict
-except ImportError:
-    install_requires.extend(['ordereddict'])
+if __name__ == '__main__':
+    install_requires = ['regex']
 
-if 'develop' in sys.argv:
-    install_requires.extend([
-        'tox',
-    ])
+    try:
+        from collections import OrderedDict
+    except ImportError:
+        install_requires.extend(['ordereddict>=1.1'])
 
-def get_version():
-    basedir = os.path.dirname(__file__)
-    with open(os.path.join(basedir, 'textile/version.py')) as f:
-        variables = {}
-        exec(f.read(), variables)
-        return variables.get('VERSION')
-    raise RuntimeError('No version info found.')
+    if 'develop' in sys.argv:
+        install_requires.extend([
+            'tox',
+        ])
 
-setup(
-    name='textile',
-    version=get_version(),
-    description='Textile processing for python.',
-    url='http://github.com/ikirudennis/python-textile',
-    packages=find_packages(),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    keywords='textile,text',
-    install_requires=install_requires,
-    test_suite='nose.collector',
-    tests_require=['nose'],
-    include_package_data=True,
-    zip_safe=False,
-)
+    def get_version():
+        basedir = os.path.dirname(__file__)
+        with open(os.path.join(basedir, 'textile/version.py')) as f:
+            variables = {}
+            exec(f.read(), variables)
+            return variables.get('VERSION')
+        raise RuntimeError('No version info found.')
 
+    setup(
+        name='textile',
+        version=get_version(),
+        description='Textile processing for python.',
+        url='http://github.com/ikirudennis/python-textile',
+        packages=find_packages(),
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+        keywords='textile,text',
+        install_requires=install_requires,
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        include_package_data=True,
+        zip_safe=False,
+    )
