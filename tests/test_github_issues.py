307b2d0f907d8fc521672c93dc7ddf67bc9c9cd3
Dennis Burke <dennisburke@prodigy.net>
2015-12-11 13:47:22 -0500

    add test for issue #20

--- tests/test_github_issues.py
+++ tests/test_github_issues.py
@@ -4,3 +4,9 @@ def test_github_issue_17():
     result = textile.textile('!http://www.ox.ac.uk/favicon.ico!')
     expect = '\t<p><img alt="" src="http://www.ox.ac.uk/favicon.ico" /></p>'
     assert result == expect
+
+def test_github_issue_20():
+    text = 'This is a link to a ["Wikipedia article about Textile":http://en.wikipedia.org/wiki/Textile_(markup_language)].'
+    result = textile.textile(text)
+    expect = '\t<p>This is a link to a <a href="http://en.wikipedia.org/wiki/Textile_%28markup_language%29">Wikipedia article about Textile</a>.</p>'
+    assert result == expect
