aae51a14eac0d578a01754e1c8f4bf20d4d2f2fa
Dennis Burke <dennisburke@prodigy.net>
2016-02-05 15:43:26 -0500

    ugh, do we even still need python2.6?

--- textile/utils.py
+++ textile/utils.py
@@ -67,9 +67,9 @@ def generate_tag(tag, content, attributes=None):
         # Python 2.6 doesn't have the tostringlist method, so we have to treat
         # it different.
         element_tag = ElementTree.tostring(element, encoding=enc)
-        element_text = re.sub(r"<\?xml version='1.0' encoding='UTF-8'\?>\n", '',
-                    element_tag)
-        if content is not ' /':
+        element_text = re.sub(r"<\?xml version='1.0' encoding='UTF-8'\?>\n",
+                '', element_tag)
+        if content is not six.text_type(' /'):
             element_text = element_tag.rstrip(' />')
             element_text = six.text_type('{0}>{1}</{2}>').format(six.text_type(
                 element_text), content, tag)
