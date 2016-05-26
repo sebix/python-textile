6aa991e2f62fbf36a8138854d45b1ce685bad60b
Dennis Burke <dennisburke@prodigy.net>
2016-05-26 13:35:42 -0400

    real fixes ???

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,10 +6,11 @@ def test_console_script():
     try:
         result = subprocess.check_output(command)
     except AttributeError:
+        command[2] = 'textile.__main__'
         result = subprocess.Popen(command,
                 stdout=subprocess.PIPE).communicate()[0]
     with open('tests/fixtures/README.txt') as f:
         expect = ''.join(f.readlines())
     if type(result) == bytes:
-        result.decode('utf-8')
+        result = result.decode('utf-8')
     assert result == expect
