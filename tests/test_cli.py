21217e0a1c129ba88f3a71b89d2e31b768989091
Dennis Burke <dennisburke@prodigy.net>
2016-05-26 13:05:49 -0400

    hopefully this will fix py26 and py3k

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,13 @@
+import six
 import subprocess
 
 def test_console_script():
-    result = subprocess.check_output(['python', '-m', 'textile', 'README.textile'])
+    command = ['python', '-m', 'textile', 'README.textile']
+    try:
+        result = subprocess.check_output(command)
+    except AttributeError:
+        result = subprocess.Popen(command,
+                stdout=subprocess.PIPE).communicate()[0]
     with open('tests/fixtures/README.txt') as f:
         expect = ''.join(f.readlines())
-    assert result == expect
+    assert six.text_type(result) == six.text_type(expect)
