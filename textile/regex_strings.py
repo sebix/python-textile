8938421ddb366925780b106373ff5917c43651e1
Dennis Burke <dennisburke@prodigy.net>
2017-09-17 10:36:08 -0400

    increase coverage

--- textile/regex_strings.py
+++ textile/regex_strings.py
@@ -3,37 +3,18 @@ from __future__ import unicode_literals
 
 import six
 
-try:
-    # Use regex module for matching uppercase characters if installed,
-    # otherwise fall back to finding all the uppercase chars in a loop.
-    import regex as re
-    upper_re_s = r'\p{Lu}'
-    regex_snippets = {
-        'acr': r'\p{Lu}\p{Nd}',
-        'abr': r'\p{Lu}',
-        'nab': r'\p{Ll}',
-        'wrd': r'(?:\p{L}|\p{M}|\p{N}|\p{Pc})',
-        'cur': r'\p{Sc}',
-        'digit': r'\p{N}',
-        'space': r'(?:\p{Zs}|\v)',
-        'char': r'(?:[^\p{Zs}\v])',
-        }
-except ImportError:
-    import re
-    from sys import maxunicode
-    upper_re_s = "".join(
-            [six.unichr(c) for c in six.moves.range(maxunicode) if six.unichr(
-                c).isupper()])
-    regex_snippets = {
-        'acr': r'{0}0-9'.format(upper_re_s),
-        'abr': r'{0}'.format(upper_re_s),
-        'nab': r'a-z',
-        'wrd': r'\w',
-        'cur': r'',
-        'digit': r'\d',
-        'space': r'(?:\s|\v)',
-        'char': r'\S',
-        }
+import regex as re
+upper_re_s = r'\p{Lu}'
+regex_snippets = {
+    'acr': r'\p{Lu}\p{Nd}',
+    'abr': r'\p{Lu}',
+    'nab': r'\p{Ll}',
+    'wrd': r'(?:\p{L}|\p{M}|\p{N}|\p{Pc})',
+    'cur': r'\p{Sc}',
+    'digit': r'\p{N}',
+    'space': r'(?:\p{Zs}|\v)',
+    'char': r'(?:[^\p{Zs}\v])',
+}
 
 halign_re_s = r'(?:\<(?!>)|(?<!<)\>|\<\>|\=|[()]+(?! ))'
 valign_re_s = r'[\-^~]'
--- textile/tools/sanitizer.py
+++ textile/tools/sanitizer.py
@@ -3,10 +3,7 @@ def sanitize(string):
     Ensure that the text does not contain any malicious HTML code which might
     break the page.
     """
-    try:
-        from html5lib import parseFragment, serialize
-    except ImportError:
-        raise Exception("html5lib not available")
+    from html5lib import parseFragment, serialize
 
     parsed = parseFragment(string)
     clean = serialize(parsed, sanitize=True, omit_optional_tags=False,
